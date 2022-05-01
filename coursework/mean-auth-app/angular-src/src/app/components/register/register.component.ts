18e2978d7ba0a410782d667c0a964d3afec47bc7
poh0 <xzeusking@gmail.com>
2022-05-01 22:05:30 +0300

    Auth service & registration

--- coursework/mean-auth-app/angular-src/src/app/components/register/register.component.ts
+++ coursework/mean-auth-app/angular-src/src/app/components/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ValidateService } from 'app/services/validate.service';
+import { AuthService } from 'app/services/auth.service';
 import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -16,7 +18,9 @@ export class RegisterComponent implements OnInit {
 
   constructor(
     private validateService: ValidateService,
-    private flashMessage: FlashMessagesService
+    private authService: AuthService,
+    private flashMessage: FlashMessagesService,
+    private router: Router
   ) { }
 
   ngOnInit() {
@@ -41,5 +45,17 @@ export class RegisterComponent implements OnInit {
       this.flashMessage.show("Please use a valid email", {cssClass: 'alert-danger', timeout: 3000})
       return false
     } 
+
+    // Register user
+    this.authService.registerUser(user).subscribe(data => {
+      if (data.success) {
+        this.flashMessage.show("You are now registered and can log in", {cssClass: 'alert-success', timeout: 3000})
+        this.router.navigate(['/login'])
+      } else {
+        this.flashMessage.show("Something went wrong", {cssClass: 'alert-danger', timeout: 3000})
+        this.router.navigate(['/register'])
+      }
+    })
   }
+
 }
